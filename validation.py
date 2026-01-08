c5a8d1799a4bb6433a7f4f9f5c9aad7e4599bb3f
Will123iam <98259777+Will123iam@users.noreply.github.com>
2026-01-08 18:36:23 +0000

    validates all imputs and dose some waiting as well

--- validation.py
+++ validation.py
@@ -1,4 +1,6 @@
 #Validates imputs
+from window import *
+import time
 
 def validate_str(value,options):
     if value in options: return True
@@ -16,3 +18,25 @@ def Score(player,computer,rounds):
         elif player == computer: return "It's a tie!"
         else: return "Computer wins!"
     print(player,"-",computer)
+
+def wait():
+    while choice_entry.get() == "":
+        win.update()
+        time.sleep(0.1)
+
+def key_press():
+    pressed = False
+
+    def set_pressed(): #need a better way of doing this
+        nonlocal pressed
+        pressed = True
+
+    win.bind("<KeyPress>",lambda event: set_pressed())
+
+    while not pressed:
+        win.update()
+        time.sleep(0.1)
+
+    clear_window()
+
+    return pressed
