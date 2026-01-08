b5dc51ca9d33f9ef77539001ac216566fb441579
Will123iam <98259777+Will123iam@users.noreply.github.com>
2026-01-08 18:36:03 +0000

    main game, mostly turned into functions

--- rock_paper_sisors.py
+++ rock_paper_sisors.py
@@ -1,58 +1,89 @@
+from validation import *
+from window import *
 import random
+import time
 
-def validate_str(value,options):
-    if value in options: return True
-    else: return False
+def loop():
+    clear_window()
+    rounds = int(rounds_entry.get())
+    p,c = 0,0
 
-def convert_int(value):
-    try: return int(value)
-    except:
-        print("Invaled input!")
-        return False
-    
-def Score(player,computer,rounds):
-    if rounds == 0:
-        if player > computer: return "Player wins!"
-        elif player == computer: return "It's a tie!"
-        else: return "Computer wins!"
-    print(player,"-",computer)
+    while rounds > 0:
+        choice_entry.set("") # Reset choice entry, preventing from contenuing till a new choice is made
+        round_winner = play_round()
+        
+        p,c = update_tally(p,c,round_winner)
+        display_score(p,c)
 
-
-options = ['rock', 'paper', 'scissors']
-rounds = False
-p,c=0,0
-
-while rounds == False:
-    rounds = input("Enter number of rounds to play: ")
-    rounds = convert_int(rounds)
-
-while rounds > 0:
-    computer = random.choice(options)
-    player = input("Enter rock, paper, or scissors: ").lower()
-
-    if validate_str(player,options) == True:
-        print("Computer:", computer," Player:", player)
-
-        if player == computer:
-            print("It's a tie!")
-        elif player == 'rock' and computer == 'scissors':
-            print("You win! Rock crushes scissors.")
-            p += 1
-        elif player == 'paper' and computer == 'rock':
-            print("You win! Paper covers rock.")
-            p += 1
-        elif player == 'scissors' and computer == 'paper':
-            print("You win! Scissors cut paper.")
-            p += 1
-        else:
-            print("You lost! Computer is better.")
-            c += 1
-
+        tk.Label(game_tab,text="Press any button to continue to next round").pack()
+        key_press()
 
         rounds -= 1
-        Score(p,c,rounds)
 
+    clear_window()
+    display_score(p,c)
+
+    #Game over message
+    tk.Label(game_tab, text="Game Over!", font=("Arial", 20)).pack()
+    tk.Label(game_tab, text="Press the hand to play again!", font=("Arial", 15)).pack()
+    rock_img_butt = tk.Button(game_tab, command=num_rounds, image=rock_img)
+    rock_img_butt.pack()
+
+
+def num_rounds():
+    clear_window()
+    num_rounds_display()
+
+    ttk.Button(game_tab, text="OK", command=loop).pack()
+    win.update()
+
+def choices():
+    return ['rock', 'paper', 'scissors']
+
+def choice_selection():
+    computer = random.choice(choices())
+
+    def rock():
+        choice_entry.set("rock")
+    def paper():
+        choice_entry.set("paper")
+    def scissors():
+        choice_entry.set("scissors")
+
+    tk.Button(game_tab, text="rock",command=rock).pack()
+    tk.Button(game_tab, text="paper",command=paper).pack()
+    tk.Button(game_tab, text="scissors",command=scissors).pack()
+
+    wait()
+    clear_window()
+    print("Computer:", computer," Player:", choice_entry.get())
+
+    return computer, choice_entry.get()
+
+
+def play_round(rock_img=rock_img, paper_img=paper_img, scissors_img=scissors_img):
+    winner = ""
+
+    computer, player = choice_selection()
+
+    display_animation(rock_img,paper_img,scissors_img)
+
+    if player == computer:
+        print("It's a tie!")
+        winner = "Tie"
+    elif player == 'rock' and computer == 'scissors':
+        print("You win! Rock crushes scissors.")
+    elif player == 'paper' and computer == 'rock':
+        print("You win! Paper covers rock.")
+    elif player == 'scissors' and computer == 'paper':
+        print("You win! Scissors cut paper.")
     else:
-        print("Invalid input! Please enter rock, paper, or scissors.")
+        print("You lost! Computer is better.")
+        winner = "Computer"
 
-print(Score(p,c,rounds))
+    if winner == "":
+        winner = "Player"
+        
+    tk.Label(game_tab, text=f"Computer: {computer} Player: {choice_entry.get()}",font=("Arial", 20)).pack() 
+    tk.Label(game_tab, text=f"{winner} is the winner!",font=("Arial", 15)).pack()
+    return winner
